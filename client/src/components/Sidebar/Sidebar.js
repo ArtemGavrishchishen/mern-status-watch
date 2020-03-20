5be6fc9406f1a2ed63de13f782d6e40c9042ca4d
Artem Gavrishchishen <artem.gavrishchishen@gmail.com>
2020-03-20 18:51:41 +0200

    sidebar fixed

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -1,23 +1,32 @@
 import React from 'react';
+import { CSSTransition } from 'react-transition-group';
 
 import Navigation from '../Navigation';
 
-import navigation from '../../configs/navigations';
 import styles from './Sidebar.module.css';
 
 const Sidebar = ({ isOpen, items = [], onClose }) => {
   return (
-    isOpen && (
+    <CSSTransition
+      in={isOpen}
+      timeout={300}
+      classNames={{
+        enter: styles.enter,
+        enterDone: styles.enterDone,
+        exit: styles.exit
+      }}
+      unmountOnExit
+    >
       <div className={styles.sidebar}>
         <div className={styles.container}>
           <ul className={styles.burger} onClick={onClose}>
             <li className={styles.icon}></li>
             <li className={styles.icon}></li>
           </ul>
-          <Navigation items={navigation} theme="sidebar" />
+          <Navigation items={items} theme="sidebar" />
         </div>
       </div>
-    )
+    </CSSTransition>
   );
 };
 
